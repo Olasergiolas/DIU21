46e256602677c3c9a76735a9a00222e9f43ad2d5
Sergio García <Olasergiolas@gmail.com>
2021-03-14 13:22:33 +0100

    Isella 2.0

--- P1/UX-DIU-Toolkit-master/Persona.js
+++ P1/UX-DIU-Toolkit-master/Persona.js
@@ -38,23 +38,41 @@ angular.module("angular", [])
 				Quote: "Si es cierto lo de que los errores te hacen más fuerte, yo sería campeona de halterofilia",
 				Age: 33,
 				Occupation: "Periodista",
-				Family: "Conoció a su pareja, Alberto, hace 6 meses. Su familia siempre está ahí para apoyarla en todo lo que necesita",
-				Location: "Armilla (Granada)",
-				Character: "Le apasiona la cocina y aprovechar su tiempo",
+				Family: "Conoció a su pareja, Alberto, hace 8 meses. Su familia siempre está ahí para apoyarla en todo lo que necesita",
+				Location: "Barcelona",
+				Character: "Le apasiona la cocina y escuchar la radio",
 				PersonalityTraits: [
 					{ Name: "Introvertido/reservado Vs  Extrov/activo ", Value: 3 },
 					{ Name: "Realista/práctico  Vs    Intuición/imaginativo", Value: 2 },
 					{ Name: "Racional/analitico  Vs   Emocional/impulsivo", Value: 4 },
 					{ Name: "Flemático/apático  Vs   Colérico/visceral", Value: 3 }
 				], 
-				Goals: ["Descubrir la cocina de otras partes del mundo", "Seguir mejorando su situación laboral"],
-				Frustrations: ["Es muy inocente y le frustra que la gente se aproveche de ello",
-				 "Le gustaría poder dedicarle más tiempo a la cocina"],
-				Bio: "Nacida en Badalona, Isella terminó estudiando periodismo en lugar de su otro hobby de toda la \
-				vida, la cocina. Isella en parte se arrepiente de esta decisión ya que encontrar un buen puesto de \
-				trabajo no fue fácil. Tras enganchar varios puestos mediocres en distintas televisiones provinciales \
-				durante años, consiguió un puesto de trabajo bien pagado en el diario Ideal de Granada, por lo que se \
-				mudó a un piso en el centro de Armilla, donde conoció a su pareja, Alberto, a través de unos compañeros de trabajo.",
+				Goals: ["Descubrir la cocina de otras partes del mundo. Tiene un especial interés por la comida asiática",
+				"Ser periodista de renombre y presentar el telediario en alguna de las principales cadenas de televisión",
+				"Remodelar el salón de su casa y hacer algo más abierto y luminoso con colores claros y plantas decorativas",
+				"Conocer a su ídolo, Iñaki Gabilondo"],
+				Frustrations: ["Es muy inocente y le frustra que la gente se aproveche de ello, sobretodo le molestan las bromas pesadas\
+				que le gastan sus compañeros de trabajo. Es por ello que está un poco quemada con su ambiente laboral actual.",
+				"Le gustaría poder dedicarle más tiempo en sus ratos libres a mejorar sus habilidades en la cocina. Su trabajo en la Vanguardia\
+				le consume todo su tiempo y energías",
+				"El ruido y bullicio que hay en el centro de Barcelona comparado con la tranquilidad de su pueblo de toda la vida"],
+				Bio: "Nacida en Martorell (Cataluña) en 1988. Es la pequeña de 2 hermanos, Bernat y ella. Bernat siempre le ha tenido un\
+				cariño especial y ha sido como un padre para ella, ya que su padre, Andreu, se pasaba los días enteros fuera de casa en la\
+				fábrica de Seat de Martorell donde él trabajaba. Clara, su madre, regentaba una panadería humilde en el pueblo. Fue allí \
+				donde Isella comenzó a interesarse desde muy pequeña por el mundo de la panadería y en general, la cocina tras pasarse \
+				incontables tardes observando a su madre amasar y hornear hogazas de pan y dulces de todo tipo por los que su madre era \
+				conocida en el pueblo. Isella tuvo una infancia normal, su familia era de clase media y sus dos padres trabajaban por lo \
+				que podían permitirse vivir sin problemas y con algún capricho ocasionalmente. En el colegio destacaba por ser muy \
+				creativa y tener una capacidad de diálogo muy superior a los niños de su edad. Una vez ya en 1º de ESO, al darse cuenta\
+				sus profesores de su facilidad para las letras, empezaron a mandarle tareas más avanzadas como la lectura de artículos \
+				periodísticos o la escucha de programas de radio informativos. Fueron estas tareas las que despertaron su interés por \
+				el periodismo. Escuchar programas de radio como el “Hoy por Hoy” de Iñaki Gabilondo o “La Radio de Julia” de Julia Otero \
+				pasó a ser uno de sus pasatiempos favoritos. Isella terminó estudiando periodismo en lugar de su otro hobby de toda la \
+				vida, la cocina. En parte, se arrepiente de esta decisión ya que encontrar un buen puesto de trabajo no le fue fácil. \
+				Tras enganchar varios puestos mediocres como redactora en distintas televisiones provinciales durante años, a los 32 \
+				consiguió un puesto de trabajo bien pagado en el diario la Vanguardia, por lo que se mudó a un piso en el centro de Barcelona, \
+				donde conoció a su pareja, Alberto, a través de unos compañeros de trabajo. Aunque se siente realizada en su puesto actual, \
+				en el futuro a Isella le gustaría dar el paso a la televisión en los telediarios de alguna de las grandes cadenas.",
 				Tech: [
 					{ Name: "TIC/Internet", Value: 2 },
 					{ Name: "Movil", Value: 3 },
@@ -62,10 +80,10 @@ angular.module("angular", [])
 					{ Name: "Software", Value: 3 }
 					
 				], 
-                Contextos: "Fue estafada organizando un viaje en el pasado por una supuesta compañía de vuelos que resultó\
-				no existir. Desde entonces, siempre que intenta hacer una reserva online, se termina saliendo a mitad del \
-				proceso por cualquier cosa que aprecie que le chirríe. Aún así, Isella lo sigue intentando porque desea \
-				realizar un viaje con su pareja.",  
+                Contextos: "Fue estafada organizando un viaje el año pasado por una supuesta compañía de vuelos que resultó\
+				no existir. Desde entonces, siempre que intenta hacer una reserva online, termina abandonando el proceso a \
+				la mitad por cualquier cosa que aprecie que le genere inseguridad. Aún así, Isella está ilusionada por organizar \
+				un viaje con su pareja.",  
 				PreferredChannels: [
 					{ Name: "Publicidad Tradicional", Value: 5 },
 					{ Name: "Online & Social Media", Value: 3 },
